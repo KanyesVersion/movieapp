b33f8ea8e1eaa76f734ff8761dd2cd24b38d00e1
Samuel Baquero <samuelbaquero@192.168.1.6>
2024-12-23 23:13:23 -0500

    updated the dropdown and added a footer

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
-const ddBtn = document.querySelector('.dd-btn');
-const ddList = document.querySelector('.dd-list');
+const ddBtns = document.querySelectorAll('.dd-btn');
+const ddLists = document.querySelectorAll('.dd-list');
 
-ddBtn.addEventListener('click', () => {
-    ddList.classList.remove('hidden');
+[...ddBtns].forEach(btn => {
+    const ddContainer = btn.parentElement;
+
+    btn.addEventListener('click', () => {
+        ddContainer.querySelector('.dd-list').classList.remove('hidden');
+        btn.style.borderRadius = '2rem 2rem 0 0';
+        btn.querySelector('.dd-chevron').classList.add('rotated-90-ccw');
+    });
 });
 
+
 document.addEventListener('click', e => {
-    [...document.querySelectorAll('.dd-list')].forEach(el => {
-        if (!el.contains(e.target) && !ddBtn.contains(e.target)) {
-            if (![...el.classList].some(cl => cl === 'hidden')) {el.classList.add('hidden');}
+    [...ddLists].forEach(list => {
+        const ddContainer = list.parentElement;
+        const assocBtn = ddContainer.querySelector('.dd-btn');
+
+        if (!list.contains(e.target) && !assocBtn.contains(e.target)) {
+            if (![...list.classList].some(cl => cl === 'hidden')) {
+                list.classList.add('hidden');
+                assocBtn.style.borderRadius = '2rem';
+                assocBtn.querySelector('.dd-chevron').classList.remove('rotated-90-ccw');
+            }
         }
     });
 });
