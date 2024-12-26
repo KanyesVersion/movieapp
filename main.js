eecf0959ea7add58d5e658cf3d86333ef8254637
Samuel Baquero <samuelbaquero@192.168.1.3>
2024-12-26 16:31:42 -0500

    improved homepage responsiveness

--- main.js
+++ main.js
@@ -3,11 +3,18 @@ const ddLists = document.querySelectorAll('.dd-list');
 
 [...ddBtns].forEach(btn => {
     const ddContainer = btn.parentElement;
+    const assocList = ddContainer.querySelector('.dd-list');
 
     btn.addEventListener('click', () => {
-        ddContainer.querySelector('.dd-list').classList.remove('hidden');
-        btn.style.borderRadius = '2rem 2rem 0 0';
-        btn.querySelector('.dd-chevron').classList.add('rotated-90-ccw');
+        if (assocList.classList.contains('hidden')) {
+            assocList.classList.remove('hidden');
+            btn.style.borderRadius = '2rem 2rem 0 0';
+            btn.querySelector('.dd-chevron').classList.add('rotated-90-ccw');
+        } else {
+            assocList.classList.add('hidden');
+            btn.style.borderRadius = '2rem';
+            btn.querySelector('.dd-chevron').classList.remove('rotated-90-ccw');
+        }
     });
 });
 
@@ -18,7 +25,7 @@ document.addEventListener('click', e => {
         const assocBtn = ddContainer.querySelector('.dd-btn');
 
         if (!list.contains(e.target) && !assocBtn.contains(e.target)) {
-            if (![...list.classList].some(cl => cl === 'hidden')) {
+            if (!list.classList.contains('hidden')) {
                 list.classList.add('hidden');
                 assocBtn.style.borderRadius = '2rem';
                 assocBtn.querySelector('.dd-chevron').classList.remove('rotated-90-ccw');
