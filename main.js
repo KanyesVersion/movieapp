b659c8a07f4ac1a15d2b847af0b115c4c202660d
Wen Dominguez <wendominguez@Wens-MacBook-Pro.local>
2024-12-27 00:05:13 -0500

    Improved collection page

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
-const ddBtn = document.querySelector('.dd-btn');
-const ddList = document.querySelector('.dd-list');
-
-ddBtn.addEventListener('click', () => {
-    ddList.classList.remove('hidden');
-});
-
-document.addEventListener('click', e => {
-    [...document.querySelectorAll('.dd-list')].forEach(el => {
-        if (!el.contains(e.target) && !ddBtn.contains(e.target)) {
-            if (![...el.classList].some(cl => cl === 'hidden')) {el.classList.add('hidden');}
-        }
-    });
+document.querySelector('.add-item-btn').addEventListener('click', () => {
+    const titles = ['The Wild Robot', 'Hannah Montana', 'Never say Never', 'Tinkerbell'];
+    const item = document.createElement('div');
+    item.classList.add('list-item');
+    item.innerHTML = `
+        <img src="./assets/BCS.jpg" alt="" class="item-img"> 
+        <h3>${titles[Math.floor(Math.random()*titles.length)]}</h3>
+        <div class="item-separator"></div>
+        <p>2015</p>
+        <div class="item-separator"></div>
+        <p>Crime/Drama</p>
+        <div class="item-separator"></div>
+        <p>Series</p>
+        <div class="item-separator"></div>
+        <div class="rating">
+            <span class="star" data-value="1">&#9733;</span>
+            <span class="star" data-value="2">&#9733;</span>
+            <span class="star" data-value="3">&#9733;</span>
+            <span class="star" data-value="4">&#9733;</span>
+            <span class="star" data-value="5">&#9733;</span>
+            <input type="hidden" id="rating-value" value="0">
+        </div>
+        <div class="item-separator"></div>
+        <button class="watched-btn">
+            <i class="fa fa-circle-o" aria-hidden="true"></i>
+        </button>
+    `;
+    document.querySelector('.list-container').appendChild(item);
 });
