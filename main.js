c2a287d7329b78a02ea74877ab99cee877c6dcb4
Samuel Baquero <samuelbaquero@192.168.1.3>
2024-12-29 00:57:08 -0500

    added recommender page and initial logic

--- main.js
+++ main.js
@@ -1,31 +1,3 @@
 import './js-utils/dropdown.js';
-
-document.querySelector('.add-item-btn').addEventListener('click', () => {
-    const titles = ['The Wild Robot', 'Hannah Montana', 'Never say Never', 'Tinkerbell'];
-    const item = document.createElement('div');
-    item.classList.add('list-item');
-    item.innerHTML = `
-        <img src="./assets/BCS.jpg" alt="" class="item-img"> 
-        <h3>${titles[Math.floor(Math.random()*titles.length)]}</h3>
-        <div class="item-separator"></div>
-        <p>2015</p>
-        <div class="item-separator"></div>
-        <p>Crime/Drama</p>
-        <div class="item-separator"></div>
-        <p>Series</p>
-        <div class="item-separator"></div>
-        <div class="rating">
-            <span class="star" data-value="1">&#9733;</span>
-            <span class="star" data-value="2">&#9733;</span>
-            <span class="star" data-value="3">&#9733;</span>
-            <span class="star" data-value="4">&#9733;</span>
-            <span class="star" data-value="5">&#9733;</span>
-            <input type="hidden" id="rating-value" value="0">
-        </div>
-        <div class="item-separator"></div>
-        <button class="watched-btn">
-            <i class="fa fa-circle-o" aria-hidden="true"></i>
-        </button>
-    `;
-    document.querySelector('.list-container').appendChild(item);
-});
+import './js-utils/addItem.js';
+import './js-utils/recommender.js';
